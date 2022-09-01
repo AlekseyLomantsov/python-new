f73c44aff0610a4403cf1c3d1e774e1b551383c9
aleksey <lomantsov92@mail.ru>
2022-09-01 13:30:00 +0300

    add dz 1

---
+++ hello world/lec.py
@@ -0,0 +1,82 @@
+# # print('hello world')
+
+# # value = None
+# # print(type(value))
+# a = 123
+# b = 1.23
+# # print(type(a))
+# # print(type(b))
+# # value = 14679
+# # print(type(value))
+
+# s = 'hello world'
+# # print(s)# вывод строки
+# print(a, b, s)
+# print(a, '-', b, '-', s)
+# print('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+# f = True
+# print(f)
+
+
+# list = [1, 2, 3, 'hello', True]
+# print(list)
+
+
+# print('Введите a')
+# a = int(input())#float(input())
+# print('Введите b')
+# b = int(input())#float(input())
+# print(a,b)
+# print('{} {}'.format(a,b))
+# print(f'{a} {b}')
+
+
+# ** возведение в степень
+# % деление с остатком
+# / для вещественных чисел
+# // для целых чисел
+# a =1.3456238855
+# b = 3
+# c = round(a * b, 6)
+# print(c)
+
+
+
+
+
+# a = 1 < 4 and 5> 2
+# a = 1 < 3 < 5< 10
+# a = 1 > 3 or 5< 10
+
+# a=[1,2,3,4]
+# # print(not 5 in a)
+# # print(2 in a)
+
+# is_odd =not a[0] % 2 
+# print(is_odd)
+
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a>b:
+#     print(a)
+# else:
+#     print(b)
+
+
+
+
+
+# list = [1,2,3,10,5]
+# for i in list:
+#     print(i)
+
+# r = range(10) не обязательно
+# for i in range(1,10,2):# от 1 до 10 в шаге 2(можно и со словами)
+#     print(i)
+
+
+
+
