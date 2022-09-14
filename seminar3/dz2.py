a2496ec9ed1ddccafdaf45ce893c1bcfb7b178f1
aleksey <lomantsov92@mail.ru>
2022-09-14 19:44:53 +0300

    add dz2 sem3

--- seminar3/dz2.py
+++ seminar3/dz2.py
@@ -7,3 +7,17 @@
 
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+my_list = [2, 3, 4, 5, 6]
+
+def mult(my_list):
+    res = []
+    if (len(my_list) % 2) == 0:
+        for i in range(len(my_list)//2):
+            res.append(my_list[i] * my_list[len(my_list)-1-i])
+    else:
+        for i in range(len(my_list)//2+1):
+            res.append(my_list[i] * my_list[len(my_list)-1-i])
+    return res
+    
+print(mult(my_list))
