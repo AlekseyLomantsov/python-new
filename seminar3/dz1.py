88dc6917e8a011ec0328c037d8b02f9cd6981076
aleksey <lomantsov92@mail.ru>
2022-09-14 18:59:19 +0300

    add task2,3 and dz1 sem 3

--- seminar3/dz1.py
+++ seminar3/dz1.py
@@ -5,3 +5,14 @@
 # Пример:
 
 # - [2, 3, 5, 9, 3] -> на нечётных индексы элементы 3 и 9, ответ: 12
+
+
+my_list = [2, 3, 5, 9, 3]
+
+def min_index(my_list):
+    min_sum = 0
+    for i in range(1, len(my_list), 2):
+        min_sum += my_list[i]
+    return min_sum
+
+print(min_index(my_list))
--- seminar3/task2.py
+++ seminar3/task2.py
@@ -5,10 +5,29 @@
 # "7"
 
 
-my_list = ['65h34q', 'sdg634d', '147jnbv']
-n = '7'
-for i in my_list:
-    if i.find(n) > 0:
-        print(i)
-    else:
-        print('none')
+# my_list = ['65h34q', 'sdg634d', '147jnbv']
+# n = '7'
+# for i in my_list:
+#     if i.find(n) >= 0:
+#         print(i)
+#     else:
+#         print('none')
+
+# listt = ["65h34q", "sdg634d", "147jnbv"]
+
+# result = [i for i in listt if '7' in i]
+# print(result)
+# if len(result) > 0:
+#     print(True)
+# else:
+#     print(False)
+
+n = 'h'
+mylist = ['jh', 'sdhfogf', 'kjahsd', '24', 'dpo', '7']
+def find_number(n, lst):
+    res = []
+    for i in lst:
+        if n in i:
+            res.append(i)
+    return res
+print(find_number(n, mylist))
