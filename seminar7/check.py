cd3f7ce30be0d11bedfcdfbe4e572cea66cc4afb
aleksey <lomantsov92@mail.ru>
2023-05-04 15:34:17 +0300

    sem7_calc+matchCase

--- seminar7/check.py
+++ seminar7/check.py
@@ -1,13 +1,26 @@
 import functions
 
 def check(data):
-    if data[1] == '+':
-        return functions.sum(data[0], data[2])
-    elif data[1] == '-':
-        return functions.minus(data[0], data[2])
-    elif data[1] == '*':
-        return functions.mult(data[0], data[2])
-    elif data[1] == '/':
-        return functions.dev(data[0], data[2])
-    else:
-        print("wrong equetion sing!")
+    # if data[1] == '+':
+    #     return functions.sum(data[0], data[2])
+    # elif data[1] == '-':
+    #     return functions.minus(data[0], data[2])
+    # elif data[1] == '*':
+    #     return functions.mult(data[0], data[2])
+    # elif data[1] == '/':
+    #     return functions.dev(data[0], data[2])
+    # else:
+    #     print("wrong equetion sing!")
+    match data[1]:
+        case '+':
+            return functions.sum(data[0], data[2])
+        case '-':
+            return functions.minus(data[0], data[2])
+        case '*':
+            return functions.mult(data[0], data[2])
+        case '/':
+            return functions.dev(data[0], data[2])
+        case _:
+            print("wrong equetion sing!")
+
+        
