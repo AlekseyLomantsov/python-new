e9b83363a1105614aa6d16701b5b705ebb3bb2b9
aleksey <lomantsov92@mail.ru>
2023-05-08 14:56:53 +0300

    +log

--- seminar7/database.py
+++ seminar7/database.py
@@ -1,12 +1,19 @@
 path = r'data.txt'
-
+path_log = f'log.txt'
 def get_info():
     with open(path, 'r') as data:
         data = data.read().split()
         return data
 
 def save_info(a):
-    b = str(a)
-    a = str(a)
     with open(path, 'w') as data:
-        data.write(a)
+        data.write(str(a))
+
+def save_result(a):
+    with open(path, 'a') as data:
+        data.write(f'\nYour result = {str(a)}')
+
+def log(a, b):
+    with open(path_log, 'a') as data:
+        # data.write(f'\nYour equation = {str(a)} = {str(b)}')
+        data.write('\nYour equation = {} {} {} = {}'.format(a[0], a[1], a[2], b))
